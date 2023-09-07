51f76b7d35386131e5aede5e815961c3da75cf64
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-09-07 10:39:48 -0300

    fix console error related to wrong import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import axios from 'axios';
 
 import Home from './view/pages/Home.js'
